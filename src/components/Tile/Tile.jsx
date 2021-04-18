00f40a7ba60fcda65e53d94be047a6e706d9a19f
geovla93 <geovla@icloud.com>
2021-04-18 17:46:57 +0300

    fixed link icon in gridlist

--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { GridListTileBar, IconButton } from "@material-ui/core";
+import { GridListTileBar, IconButton, Link } from "@material-ui/core";
 import InfoIcon from "@material-ui/icons/Info";
 import LinkIcon from "@material-ui/icons/Link";
 
@@ -33,14 +32,14 @@ const Tile = ({ tile }) => {
 						>
 							<InfoIcon />
 						</IconButton>
-						<IconButton
-							aria-label={`Link to ${tile.title}`}
-							className={classes.icon}
-						>
-							<LinkIcon>
-								<Link to={tile.path} />
-							</LinkIcon>
-						</IconButton>
+						<Link href={tile.path} target="_blank" rel="noopener noreferrer">
+							<IconButton
+								aria-label={`Link to ${tile.title}`}
+								className={classes.icon}
+							>
+								<LinkIcon />
+							</IconButton>
+						</Link>
 					</>
 				}
 			/>
