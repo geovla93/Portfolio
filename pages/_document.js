afe78e7b8eadd9117b97873256db7acbd1389616
geovla93 <geovla@icloud.com>
2021-05-27 09:40:19 +0300

    change project info

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,8 @@ class MyDocument extends Document {
 					<link
 						rel="stylesheet"
 						type="text/css"
-						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700&display=swap|Material+Icons"
+						media="all"
+						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons&display=swap"
 					/>
 				</Head>
 				<body>
