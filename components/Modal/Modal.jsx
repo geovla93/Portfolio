527531ceaa5f30605195c69f063433e6f1e0b5b4
geovla93 <geovla@icloud.com>
2021-06-16 16:05:07 +0300

    modal fixed

--- components/Modal/Modal.jsx
+++ components/Modal/Modal.jsx
@@ -10,7 +10,7 @@ const ModalContent = ({ open, tile, onClose }) => {
 			{open ? (
 				<>
 					<div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-						<div className="relative w-2/5 my-6 mx-auto max-w-3xl">
+						<div className="relative w-4/5 md:w-3/5 lg:2/5 xl:1/5 my-6 mx-auto max-w-3xl">
 							{/*content*/}
 							<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
 								{/*header*/}
